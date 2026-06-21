1f69200dbc4c321791040a0f27e6103e3b465d0a
sara-ads <sara.avila.sousa16@gmail.com>
2026-06-21 14:42:40 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldMessenger(
+      child: MaterialApp(
+        title: 'Gerenciador de Tarefas Diárias',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen(),
+      ),
+    );
+  }
+}
